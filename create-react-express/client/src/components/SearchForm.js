a9a83b6b4fa6ba0ec1ea1140e7510f5e0a36cdf8
bwillstorey <bwillstorey@gmail.com>
2019-06-09 16:55:43 -0500

    completed SearchForm and RadioButton components

---
+++ create-react-express/client/src/components/SearchForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import RadioButton from "./RadioButton";
+
+const SearchForm = ({
+    inputValue,
+    radioButtons,
+    handleTextChange,
+    handleRadioChange,
+    selectedOption,
+    handleFormSubmit
+}) => (
+    <form>
+
+        {radioButtons.map(radio => (
+            <RadioButton 
+                key={`radio-${radio}`}
+                value={radio}
+                selectedOption={selectedOption}
+                handleChange={handleRadioChange}
+            />
+        ))}
+
+        <label>
+            Book Name
+            <input 
+                type="text"
+                onChange={handleTextChange}
+                value={inputValue}
+            />
+        </label>
+
+        <button onClick={handleFormSubmit}>
+            Search
+        </button>
+    </form>
+)
+
+export default SearchForm;
