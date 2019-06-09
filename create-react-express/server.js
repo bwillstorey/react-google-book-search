eed1486c391b7d5af290294ac7e608fe086fb28e
bwillstorey <bwillstorey@gmail.com>
2019-06-09 14:04:01 -0500

    initial app structure. configured server.js

---
+++ create-react-express/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true })); //true or false?
+app.use(bodyParser.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// setup routing
+const routes = require("./routes");
+app.use(routes);
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
